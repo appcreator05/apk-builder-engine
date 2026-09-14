da97accde919327c8198a75814a44402ecf18072
appcreator05 <installapkapps@gmail.com>
2026-09-14 19:52:42 +0530

    Set up WebView in MainActivity
    Initialize WebView with JavaScript and DOM storage enabled, and load a target URL.

--- app/src/main/java/com/creator/app/MainActivity.java
+++ app/src/main/java/com/creator/app/MainActivity.java
@@ -1,1 +1,38 @@
+package com.creator.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // GitHub Actions এই লেখাটিকে ইউজারের দেওয়া URL দিয়ে বদলে দেবে
+        String targetUrl = "DEFAULT_URL_HERE";
+        webView.loadUrl(targetUrl);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
